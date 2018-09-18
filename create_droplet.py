0722b46c30307f73db6cb4596e90682fa3641a80
Noah Studach <nstudach@gmail.com>
2018-09-18 15:36:12 +0200

    Removed references to install_script Removed unused delete_VM function

--- create_droplet.py
+++ create_droplet.py
@@ -6,17 +6,7 @@ from pssh.clients import ParallelSSHClient
 from pssh.utils import enable_logger, logger
 from gevent import joinall
 
-def destroy_VM(headers,id):
-    #REMOVE destroy part
-    input("Enter anything to destroy VM: " + str(id))
-    
-    url = "https://api.digitalocean.com/v2/droplets/" + str(id)
-    
-    # delete droplet
-    r = requests.delete(url, headers=headers)
-    print(r)
-
-def ssh_stuff(hosts, jobs, jobs_setup, inputfiles):
+def ssh_stuff(hosts, jobs, inputfiles):
     '''
     Copys local files to remote hosts
     Installs packages and libraries
@@ -35,8 +25,6 @@ def ssh_stuff(hosts, jobs, jobs_setup, inputfiles):
 
     # copy install_script, run_pathspider, input file
     print('start copying files')
-    cmds = client.copy_file('install_script.py', 'install_script.py')
-    joinall(cmds, raise_error=True)
     cmds = client.copy_file('run_pathspider.py', 'run_pathspider.py')
     joinall(cmds, raise_error=True)
     cmds = client.copy_file('config.json', 'config.json')
@@ -47,15 +35,9 @@ def ssh_stuff(hosts, jobs, jobs_setup, inputfiles):
             joinall(cmds, raise_error=True)
     
     sleep(10)
-    
-    #remote execute install_script
-    print('Setting up VMs')
-    output = client.run_command('%s', host_args = jobs_setup)
-    #wait until finished
-    client.join(output, consume_output=False, timeout=None)
-    
+
     #remote execute run_pathspider
-    print('Initiate measurement scripts')
+    print('Run deployment script')
     output = client.run_command('%s', host_args = jobs, use_pty = False)
     
     sleep(20)
@@ -116,11 +98,9 @@ if __name__ == "__main__":
     droplet_config = config['droplet']
     regions = config['provider']['regions']
     headers = config['provider']['headers']
-    inputfile = config['measurement']['inputfile']
     
     hosts = []
     jobs = []
-    jobs_setup = []
     ids = []
     
     for region in regions:
@@ -130,7 +110,6 @@ if __name__ == "__main__":
         print(name+':'+ip)
         hosts.append(ip)
         jobs.append('setsid python3 run_pathspider.py ' +name+' '+plugin+' '+str(id))
-        jobs_setup.append('python3 install_script.py ' + name)
         ids.append(id)
         
     # for testing purpouse
@@ -142,7 +121,5 @@ if __name__ == "__main__":
     #may want to wait a bit for host to start
     sleep(50)
 
-    ssh_stuff(hosts, jobs, jobs_setup, inputfile)
+    ssh_stuff(hosts, jobs, config['measurement']['inputfile'])
     print('Disconnected from hosts! Progress will be displayed on the slack channel.')
-    # for id in ids:
-        # destroy_VM(headers, id)
