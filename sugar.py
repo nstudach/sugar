9ffebb71097fd35c8b58e7f1462c3d51991ade3c
Noah Studach <nstudach@gmail.com>
2018-09-21 21:40:35 +0200

    Restructured to fit modular execution Removed debug output Saved droplet data in config

--- sugar.py
+++ sugar.py
@@ -6,26 +6,33 @@ from pssh.clients import ParallelSSHClient
 from pssh.utils import enable_logger, logger
 from gevent import joinall
 
-def ssh_stuff(hosts, jobs, inputfiles):
-    '''
-    Copys local files to remote hosts
-    Installs packages and libraries
-    Executes pathspider in servers background
-    Disconnects ssh_connection
-    
-    :param hosts: Contains IP adresses of VM machines
-    :type hosts: list(str)
-    :param jobs: Contains Commands for VM machines (orderd)
-    :type jobs: list(str)
-    :param inputfile: Location of pathspider jobs ndjson file
-    '''
+def send_ssh(hosts, jobs_setup, copy, inputfiles):
     enable_logger(logger)
-    
     client = ParallelSSHClient(hosts, user = 'root', pkey = 'keys/id_rsa')
 
-    # copy remote_script, input file, config file
-    print('start copying files')
+    if copy:
+        print('Start copying files')
+        copy_files(client, inputfiles)
+        sleep(10)
+
+    print('Updating configs')
+    if type(jobs_setup) == list:
+        output = client.run_command('%s', host_args = jobs_setup)
+        client.join(output, consume_output=False, timeout=None)
+    else:
+        output = client.run_command(jobs_setup)
+        client.join(output, consume_output=False, timeout=None)
+    #execute remote_script
+    print('Starting deployment script')
+    # client.run_command('setsid python3 remote_script.py', use_pty = False)
+    
+    sleep(20)
+
+def copy_files(client, inputfiles):
+    # copy remote_script, input files, config file
     cmds = client.copy_file('remote_script.py', 'remote_script.py')
+    joinall(cmds, raise_error=True)
+    cmds = client.copy_file('update_config.py', 'update_config.py')
     joinall(cmds, raise_error=True)
     cmds = client.copy_file('config.json', 'config.json')
     joinall(cmds, raise_error=True)
@@ -33,22 +40,51 @@ def ssh_stuff(hosts, jobs, inputfiles):
         if not inputfile.startswith('http'):
             cmds = client.copy_file(inputfile, inputfile)
             joinall(cmds, raise_error=True)
+
+def get_info_from_config(config):
+    try:
+        host_info = config['setup']['host info']
+    except:
+        print('No host info found. Create Droplets first')
+        return False
+    try:
+        hosts = [ip for name, ip, id in host_info]
+        if config['setup']['install']:
+            copy = True
+            jobs_setup = ['python3 update_config.py ' + 'name=' + name + ' id=' + str(id) for name, ip, id in host_info]
+            print(jobs_setup)
+        else:
+            #update config
+            copy = False
+            jobs_setup = ['python3', 'update_config.py']
+            for tag in ['debug', 'install', 'measure', 'upload', 'destroy']:
+                jobs_setup.append(tag + '=' + config['setup'][tag])
+            jobs_setup = ' '.join(jobs_setup)
+            print(jobs_setup)
+    except:
+        print('Something wrong with config.json.\nCould not read host info')
+        print(host_info)
+        return False
+    return (hosts, jobs_setup, copy)
+
+def setup_droplets(config, plugin):
+    #create droplets and save ip adresses and ids
+    droplet_config = config['droplet']
+    regions = config['provider']['regions']
+    headers = config['provider']['headers']
     
-    sleep(10)
-    
-    print('Letting the machine sleep')
-    output = client.run_command('sleep 2m')
-    #wait until finished
-    client.join(output, consume_output=False, timeout=None)
-    
-    #remote execute remote_script
-    print('Starting deployment script')
-    print(jobs)
-    client.run_command('%s', host_args = jobs, use_pty = False)
-    
-    sleep(20)
-    
-    print('closing connections')
+    host_info = []
+
+    for region in regions:
+        id, name = create_VM(headers, region, plugin, droplet_config)
+        time.sleep(10)
+        ip = get_IP(headers, id)
+        print(name+':'+ip)
+        if ip != 'no ip':
+            host_info.append([name, ip, id])
+        else:
+            print(name + ': IP could not be retrieved. Droplet destroyed')
+    return host_info
 
 def create_VM(headers, region, plugin, droplet_config):
     #base droplet configuration
@@ -79,47 +115,39 @@ def get_IP(headers, id):
     try:
         return data['droplet']['networks']['v4'][0]['ip_address']
     except:
-        print(data)
-        print ('retry')
-        sleep(20)
-        r = requests.get(url, headers=headers)
-        data= r.json()
-        return data['droplet']['networks']['v4'][0]['ip_address']
+        for _ in range(3):
+            print ('Digital Ocean not yet ready.\nretry')
+            sleep(20)
+            r = requests.get(url, headers=headers)
+            data= r.json()
+            try:
+                return data['droplet']['networks']['v4'][0]['ip_address']
+            except:
+                pass
+        #kill droplet
+        requests.delete(url, headers=headers)
+        return 'no ip'
 
 def sleep(seconds):
     print('Sleeping for %d sec' % seconds)
     time.sleep(seconds)
 
 if __name__ == "__main__":
-        
-    plugin = str(sys.argv[1])
     #read_conf
     config = json.load(open('config.json'))
-    droplet_config = config['droplet']
-    regions = config['provider']['regions']
-    headers = config['provider']['headers']
     
-    hosts = []
-    jobs = []
-    ids = []
-    
-    for region in regions:
-        id, name = create_VM(headers, region, plugin, droplet_config)
-        time.sleep(10)
-        ip = get_IP(headers, id)
-        print(name+':'+ip)
-        hosts.append(ip)
-        jobs.append('setsid python3 remote_script.py ' + name + ' ' + str(id))
-        ids.append(id)
-        
-    # for testing purpouse
-    with open('delete_commands_'+plugin, 'a') as fuckup:
-        fuckup.write(str(hosts) + '\n')
-        for id in ids:
-            fuckup.write('curl -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $KEY" "https://api.digitalocean.com/v2/droplets/'+str(id)+'"\n')
-
-    #may want to wait a bit for host to start
-    sleep(50)
-
-    ssh_stuff(hosts, jobs, config['measurement']['inputfile'])
-    print('Disconnected from hosts! Progress will be displayed on the slack channel.')
+    if config['setup']['create']:
+        host_info = setup_droplets(config, str(sys.argv[1]))
+        # writing host inforamtion in config
+        config['setup']['host info'] = host_info
+        json.dump(config, open('config.json', 'w'), indent=4)   
+        print ('Created droplets.')
+        if config['setup']['install']:
+            sleep(50)
+    if config['setup']['install'] or config['setup']['measure'] or config['setup']['upload'] or config['setup']['destroy']:
+        try:
+            hosts, jobs_setup, copy = get_info_from_config(config)
+            send_ssh(hosts, jobs_setup, copy, config['measurement']['inputfile'])
+            print('Disconnected from hosts! Progress will be displayed on the slack channel.')
+        except:
+            pass
